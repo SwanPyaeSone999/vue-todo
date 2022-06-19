839919cf44095ef5285247bd2467f58d4e45fdfb
Swan Pyae Sone <swanpyae.sone318@gmail.com>
2022-06-18 22:49:33 -0700

    vue-animation-todo

---
+++ src/views/About.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="about">
+   <transition
+    appear
+   name="about" 
+   @before-enter="beforeEnter"
+   @enter="enter"
+   @after-enter="afterEnter"
+   >
+     <h1 >About</h1>
+   </transition>
+   
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+   
+  </div>
+</template>
+
+<script>
+import  ref  from 'vue';
+import gsap from 'gsap'
+export default {
+  setup() {
+     
+    const beforeEnter = (el) =>{
+      console.log("beforeEnter -set inital state",el);
+      el.style.transform  = 'translateY(-60px)';
+      el.style.opaticy = 0; 
+    }
+    const enter = (el,done) =>{
+      console.log("enter");
+      gsap.to(el,{
+        duration:3,
+        y:0,
+        opacity:1,
+        ease: 'bounce.out',
+        onComplete: done
+      })
+    }
+    const afterEnter = () =>{
+      console.log("afterneter")
+    }
+  
+    return{beforeEnter,enter,afterEnter}
+  }
+}
+</script>
+
+<style>
+  .about {
+    max-width: 600px;
+    margin: 20px auto;
+  }
+
+  /* .about-enter-from{
+    opacity: 0;
+  }
+  .about-enter-active{
+    transition: all 1s ease;
+  } */
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <transition name="fade">
+      <Toast v-if="showToast" />
+    </transition>
+    <Todos @badValue="triggerToast" />
+  </div>
+ 
+</template>
+
+
+<script>
+import { ref } from 'vue'
+import Toast from '../components/Toast'
+import Todos from '../components/Todos'
+
+export default {
+  components: { Toast, Todos },
+  setup() {
+    const showToast = ref(false)
+    const showP = ref(false)
+
+    const triggerToast = () => {
+      showToast.value = true;
+      setTimeout(() => showToast.value = false, 3000)
+    }
+
+    return { showToast, triggerToast ,showP}
+  }
+}
+</script>
+
+<style>
+    /* .fade-enter-from{
+      opacity: 0;
+      transform: translateY(-60px);
+     
+    } */
+    .fade-enter-active{
+       animation: wobble 0.5s ease;
+    }
+
+    .fade-leave-from{
+      opacity: 1;
+      transform: translateY(0);
+    }
+    .fade-leave-to{
+      opacity: 0;
+      transform: translateY(-60px);
+    }
+    .fade-leave-active{
+        transition: all 1s ease;
+    }
+
+
+    @keyframes wobble {
+      0%{ transform: translateY(-60px);opacity: 0;}
+      50%{ transform: translateY(0px);opacity: 1;}
+      60%{ transform: translateX(8px);}
+      70%{ transform: translateX(-8px);}
+      80%{ transform: translatex(8px);}
+      90%{ transform: translateX(-8px);}
+      100%{transform: translateX(0);}
+    }
+</style>
